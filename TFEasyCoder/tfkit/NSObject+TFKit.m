7ad62b8cc881373ecbf25f35c63682c8d0d2cb37
zhutaofeng <zhutaofeng@sdiholding.com>
2019-06-10 18:58:17 +0800

    修改方法交换的bug

--- TFEasyCoder/tfkit/NSObject+TFKit.m
+++ TFEasyCoder/tfkit/NSObject+TFKit.m
@@ -260,88 +260,31 @@ static id _shareInstance;
  *  交换类方法和交换实例方法
  */
 +(BOOL)tf_code_pre(instanceMethodExchange):(SEL)originSel toClass:(Class)toClass  toSel:(SEL)toSel{
-    //    tf_exchangeLock();
-    //    if (tf_methodHasExchanged(cls, originSel) == NO) {
     Method originMethod = class_getInstanceMethod([self class], originSel);
     Method toMethod = class_getInstanceMethod(toClass, toSel);
-    if (originMethod && toMethod) {
+    BOOL added = class_addMethod([self class], originSel, method_getImplementation(toMethod), method_getTypeEncoding(toMethod));
+    if (!added) {
         method_exchangeImplementations(originMethod, toMethod);
-        NSLog(@"交换成功:\n%@:\n%@:\n%@:\n%@",
-              NSStringFromClass([self class]),
-              NSStringFromSelector(originSel),
-              NSStringFromClass(toClass),
-              NSStringFromSelector(toSel));
     }else{
-        NSLog(@"交换失败:%@:%@:%@:%@",
-              NSStringFromClass([self class]),
-              NSStringFromSelector(originSel),
-              NSStringFromClass(toClass),
-              NSStringFromSelector(toSel));
+        class_replaceMethod(self, toSel, method_getImplementation(originMethod), method_getTypeEncoding(originMethod));
     }
-    
-    return YES;
-    
-    IMP toImp = method_getImplementation(toMethod);
-    const char *toTypeEncoding = method_getTypeEncoding(toMethod);
-    BOOL addMethod = class_addMethod([self class],originSel,toImp,toTypeEncoding);
-    
-    if (addMethod) {
-        //            IMP originImp = method_getImplementation(originMethod);
-        //            const char *originTypeEncoding = method_getTypeEncoding(originMethod);
-        //            class_replaceMethod([self class],toSel,originImp,originTypeEncoding);
-        //            tf_methodExchangeRecord(cls, originSel);
-    }else{
-        if (originMethod && toMethod){
-            method_exchangeImplementations(originMethod, toMethod);
-            //                tf_methodExchangeRecord(cls, originSel);
-        }
-    }
-    //    }
-    //    tf_exchangeUnlock();
     return YES;
 }
 
 +(BOOL)tf_code_pre(classMethodExchange):(SEL)originSel toClass:(Class)toClass  toSel:(SEL)toSel{
-    //    tf_exchangeLock();
-    //    if (tf_methodHasExchanged(cls, originSel) == NO) {
-    
     Method originMethod = class_getClassMethod([self class], originSel);
     Method toMethod = class_getClassMethod(toClass, toSel);
-    
-    if (originMethod && toMethod) {
+    Class metaClass = objc_getMetaClass(NSStringFromClass([self class]).UTF8String);
+    IMP toImp = method_getImplementation(toMethod);
+    const char *toTypeEncoding = method_getTypeEncoding(toMethod);
+    BOOL addMethod = class_addMethod(metaClass,originSel,toImp,toTypeEncoding);
+    if (!addMethod) {
         method_exchangeImplementations(originMethod, toMethod);
-        NSLog(@"交换成功:\n%@:\n%@:\n%@:\n%@",
-              NSStringFromClass([self class]),
-              NSStringFromSelector(originSel),
-              NSStringFromClass(toClass),
-              NSStringFromSelector(toSel));
     }else{
-        NSLog(@"交换失败:%@:%@:%@:%@",
-              NSStringFromClass([self class]),
-              NSStringFromSelector(originSel),
-              NSStringFromClass(toClass),
-              NSStringFromSelector(toSel));
+        IMP originImp = method_getImplementation(originMethod);
+        const char *originTypeEncoding = method_getTypeEncoding(originMethod);
+        class_replaceMethod([self class],toSel,originImp,originTypeEncoding);
     }
-    
-    //        return YES;
-    //
-    //        Class metaClass = objc_getMetaClass(NSStringFromClass(cls).UTF8String);
-    //        IMP toImp = method_getImplementation(toMethod);
-    //        const char *toTypeEncoding = method_getTypeEncoding(toMethod);
-    //        BOOL addMethod = class_addMethod(metaClass,originSel,toImp,toTypeEncoding);
-    //        if (addMethod) {
-    //            IMP originImp = method_getImplementation(originMethod);
-    //            const char *originTypeEncoding = method_getTypeEncoding(originMethod);
-    //            class_replaceMethod(cls,toSel,originImp,originTypeEncoding);
-    //            tf_methodExchangeRecord(cls, originSel);
-    //        }else{
-    //            if (originMethod && toMethod){
-    //                method_exchangeImplementations(originMethod, toMethod);
-    //                tf_methodExchangeRecord(cls, originSel);
-    //            }
-    //        }
-    //    }
-    //    tf_exchangeUnlock();
     return YES;
 }
 
