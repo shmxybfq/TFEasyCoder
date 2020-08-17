83096b65ac41ad3a9b54c2328de562b8755fed2b
zhutaofeng <zhutaofeng@qing-jin.com>
2020-08-17 14:18:04 +0800

    添加BaseTableViewCell

---
+++ TFEasyCoder/base/BaseTableViewCell.h
@@ -0,0 +1,46 @@
+//
+//  BaseTableViewCell.h
+//  HeroCoinSDK
+//
+//  Created by admin on 2018/5/16.
+//  Copyright © 2018年 zhutaofeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger,CellSourceType) {
+    CellSourceTypeCode,
+    CellSourceTypeXib,
+};
+
+typedef NS_ENUM(NSInteger, CoinTradeDetailCellTitleType) {
+    CellTitleType_ToAddress = 0,
+    CellTitleType_FromAddress,
+    CellTitleType_GasFee,
+    CellTitleType_Memo,
+    CellTitleType_TxHash,
+    CellTitleType_Block,
+    CellTitleType_Timestamp,
+} ;
+
+@interface BaseTableViewCell : UITableViewCell
+
+@property(nonatomic,strong)NSIndexPath *indexPath;
+@property(nonatomic,  weak)id delegate;
+
+
+//初始化子试图，子类需要重写
+-(void)initView;
+//设置数据，子类需要重写
+-(void)setData:(id)data;
+-(void)setData:(id)data indexPath:(NSIndexPath *)indexPath;
+-(void)setData:(id)data indexPath:(NSIndexPath *)indexPath delegate:(id)delegate;
+
+//获取手动计算高度,子类需要重写
++ (CGFloat)getManualHeightWithTableView:(UITableView *)tableView cellSourceType:(CellSourceType)type;
+//获取手动计算高度,子类不重写
++ (CGFloat)getAutoHeightWithTableView:(UITableView *)tableView cellSourceType:(CellSourceType)type;
+//获取cell
++(instancetype)dequeueReusableCell:(UITableView *)tableView cellSourceType:(CellSourceType)type;
+
+@end
---
+++ TFEasyCoder/base/BaseTableViewCell.m
@@ -0,0 +1,94 @@
+//
+//  BaseTableViewCell.m
+//  HeroCoinSDK
+//
+//  Created by admin on 2018/5/16.
+//  Copyright © 2018年 zhutaofeng. All rights reserved.
+//
+
+#import "BaseTableViewCell.h"
+
+@implementation BaseTableViewCell
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        [self initView];
+    }
+    return self;
+}
+
+-(void)initView{
+    
+    self.selectionStyle = UITableViewCellSelectionStyleNone;
+    self.backgroundColor = [UIColor whiteColor];
+}
+-(void)setData:(id)data{
+    
+}
+-(void)setData:(id)data indexPath:(NSIndexPath *)indexPath{
+    self.indexPath = indexPath;
+}
+-(void)setData:(id)data indexPath:(NSIndexPath *)indexPath delegate:(id)delegate{
+    self.indexPath = indexPath;
+    self.delegate = delegate;
+    
+    if(@available(iOS 9.0, *)) {
+        [self sendSubviewToBack:self.contentView];
+    }
+}
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    [self initView];
+}
+
++ (CGFloat)getManualHeightWithTableView:(UITableView *)tableView cellSourceType:(CellSourceType)type{
+    return 44;
+}
+
++ (CGFloat)getAutoHeightWithTableView:(UITableView *)tableView cellSourceType:(CellSourceType)type{
+    if ([UIDevice currentDevice].systemVersion.floatValue >= 8.0) {
+        return UITableViewAutomaticDimension;
+    }else{
+        UITableViewCell *cell = [self dequeueReusableCell:tableView cellSourceType:type];
+        [cell setNeedsUpdateConstraints];
+        [cell updateConstraintsIfNeeded];
+        [cell setNeedsLayout];
+        [cell layoutIfNeeded];
+        return [cell.contentView
+                systemLayoutSizeFittingSize:UILayoutFittingCompressedSize].height;
+    }
+}
+
++(instancetype)dequeueReusableCell:(UITableView *)tableView cellSourceType:(CellSourceType)type{
+    NSString *cellId = NSStringFromClass([self class]);
+    id cell =  [tableView dequeueReusableCellWithIdentifier:cellId];
+    if (cell == nil) {
+        switch (type) {
+            case CellSourceTypeCode:{
+                cell = [[[self class] alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+            }break;
+            case CellSourceTypeXib:{
+                cell = [self loadFromNibWithTableView:tableView nibName:NSStringFromClass([self class])];
+            }break;
+            default:break;
+        }
+    }
+    return cell;
+}
+
++(id)loadFromNibWithTableView:(UITableView *)tableView nibName:(NSString *)nibName{
+    UINib *nib = [UINib nibWithNibName:nibName bundle:nil];
+    [tableView registerNib:nib forCellReuseIdentifier:NSStringFromClass([self class])];
+    return [tableView dequeueReusableCellWithIdentifier:NSStringFromClass([self class])];
+}
+
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+    
+    // Configure the view for the selected state
+}
+
+@end
